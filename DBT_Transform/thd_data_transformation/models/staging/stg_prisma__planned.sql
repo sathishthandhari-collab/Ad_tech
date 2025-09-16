02490596bbf8fad68c363e11abe545cde8724d2d
Sathish Thandhari <sathishthandhari@gmail.com>
2025-09-16 16:04:31 +0530

    stag

---
+++ DBT_Transform/thd_data_transformation/models/staging/stg_prisma__planned.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='table') }}
+
+select 
+        DAY AS DATE,
+        CAMPAIGN_NAME,
+        SPLIT_PART(CAMPAIGN_NAME, '_', 4) AS CAMPAIGN_GROUP,
+        CAMPAIGN_ID AS CM360_CAMPAIGN_ID,
+        SITE_NAME,
+        PLACEMENT_NAME,
+        CREATIVE_TYPE,
+        IMPRESSIONS * uniform(0.8632, 1.265, random()) as planned_impressions
+from {{source('cm360', 'stg_cm360_raw_data')}}
