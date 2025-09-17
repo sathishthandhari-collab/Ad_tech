9340e11c82eb75194fedeadd9d5e63ad3ade89db
Sathish Thandhari <sathishthandhari@gmail.com>
2025-09-18 01:15:05 +0530

    hah

--- DBT_Transformation/adtech_data_transformation/analysis/model_yml.sql
+++
@@ -1,5 +0,0 @@
-
-{% set models_to_generate = codegen.get_models(directory='C:\Users\Sathish\OneDrive\Desktop\DA\Projects\Ad_tech\DBT_Transformation\adtech_data_transformation\models\intermediate\int_business_analytics__model_eph', prefix='int_') %}
-{{ codegen.generate_model_yaml(
-    model_names = models_to_generate
-) }}
